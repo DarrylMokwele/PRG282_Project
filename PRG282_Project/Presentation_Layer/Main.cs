5a8553a722cc7b79fb42916fa0a6ebeb20ff24c8
King <King@DESKTOP-BNGI95J>
2025-10-21 20:48:29 +0200

    Add project files.

---
+++ PRG282_Project/Presentation_Layer/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG282_Project
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Update update = new Update();
+                update.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening update form: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Delete delete = new Delete();
+                delete.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening delete form: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                addHero addHero = new addHero();
+                addHero.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening add hero form: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Summary summary = new Summary();
+                summary.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening summary form: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Superheros superheros = new Superheros();
+                superheros.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening superheros form: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ PRG282_Project/Presentation_Layer/Update.cs
@@ -0,0 +1,48 @@
+﻿using PRG282_Project.Database_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using PRG282_Project.Presentation_Layer;
+
+namespace PRG282_Project
+{
+    public partial class Update : Form
+    {
+        public Update()
+        {
+            InitializeComponent();
+        }
+
+        private void Update_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Info superhero = new Info();
+                superhero.UpdateHero(comboBox1.Text, textBox2.Text, textBox1.Text);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating superhero: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
